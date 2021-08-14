3b9d8588bdb2ff2b02f0530c361a89a64cca20a7
AhmedMostafa206 <ahmedmostafa20619@gmail.com>
2021-08-14 18:44:58 +0200

    api is ready

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -19,6 +19,7 @@ export class WeatherService {
 
 
   updateWeather() {
+    console.log('update');
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
